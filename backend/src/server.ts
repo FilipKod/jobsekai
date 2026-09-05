251f511c73cc16b812ac45a320904662d0c8446d
Filip Madunicky <yelidric@gmail.com>
2026-09-05 17:35:05 +0200

    feat: add Docker configuration for backend and frontend services

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,16 +3,16 @@ import Fastify from "fastify";
 const server = Fastify({ logger: true });
 
 server.get("/", async (request, reply) => {
-  return { hello: "hello world" };
+  return { hello: "world" };
 });
 
 const start = async () => {
   try {
-    await server.listen({ port: 4321 });
+    await server.listen({ port: 4321, host: "0.0.0.0" });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
   }
 };
 
-start();``
+start();
---
+++ frontend/dev.dockerfile
@@ -0,0 +1,13 @@
+FROM node:22-alpine
+
+WORKDIR /app
+
+COPY package*.json pnpm-lock.yaml ./
+
+RUN corepack enable && pnpm --version && pnpm install --frozen-lockfile
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["pnpm", "dev"]
