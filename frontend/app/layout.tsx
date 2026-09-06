372f8415e16395dcf2b55061b60cade4900bb544
Filip Madunicky <yelidric@gmail.com>
2026-09-06 20:11:19 +0200

    Refactor code structure for improved readability and maintainability

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,13 +1,56 @@
 import "./globals.css";
+import { Archivo_Black, Outfit } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const archivoBlack = Archivo_Black({
+  subsets: ["latin"],
+  variable: "--font-archivo-black",
+  weight: "400",
+});
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  variable: "--font-outfit",
+});
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className="h-full antialiased bg-amber-200">
-      <body className="min-h-full flex flex-col bg-amber-600">
-        <header className="bg-amber-400 p-4">
-          <h1 className="text-2xl font-bold">Jobsekai</h1>
+    <html
+      lang="en"
+      className={cn(
+        "h-full antialiased",
+        "font-outfit",
+        archivoBlack.variable,
+        outfit.variable,
+      )}
+    >
+      <body className="min-h-full flex flex-col">
+        <header className="px-12 py-3 border-b border-border flex">
+          <h1 className="text-lg font-bold uppercase font-archivo-black">
+            Jobsekai
+          </h1>
+
+          <nav className="ml-auto">
+            <ul className="flex space-x-4">
+              <li>
+                <a href="#" className="hover:underline text-sm">
+                  How it works
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline text-sm">
+                  Ranks
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline text-sm">
+                  Changelog
+                </a>
+              </li>
+            </ul>
+          </nav>
         </header>
-        <main>{children}</main>
+        <main className="px-12 py-8">{children}</main>
       </body>
     </html>
   );
